fa975ee01cc7e439edb6a73de92357d06f2f4b9a
eunyoung <eun970923@naver.com>
2021-04-27 00:01:22 +0900

    도전 과제완료

--- kakao_clone/kakao_clone/Home2ViewController.swift
+++ kakao_clone/kakao_clone/Home2ViewController.swift
@@ -13,13 +13,34 @@ class Home2ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        swipeRecognizer()
         // Do any additional setup after loading the view.
     }
     
     @IBAction func ProfileClose(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
+    
+    
+    func swipeRecognizer() {
+            let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture(_:)))
+            swipeDown.direction = UISwipeGestureRecognizer.Direction.down
+            self.view.addGestureRecognizer(swipeDown)
+            
+        }
+        
+        @objc func respondToSwipeGesture(_ gesture: UIGestureRecognizer){
+            if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+                switch swipeGesture.direction{
+                case UISwipeGestureRecognizer.Direction.down:
+                    // 스와이프 시, 원하는 기능 구현.
+                    self.dismiss(animated: true, completion: nil)
+                default: break
+                }
+            }
+        }
+
+
     
     /*
     // MARK: - Navigation
